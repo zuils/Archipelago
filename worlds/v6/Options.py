9bca6d8e0dfc118d27bdf9648e4de911c6e40d7c
zuils <vijaylemon05@gmail.com>
2025-02-12 07:36:15 -0500

    Update to 0.6.0 RC1

--- worlds/v6/Options.py
+++ worlds/v6/Options.py
@@ -2,16 +2,19 @@ import typing
 from dataclasses import dataclass
 from Options import Option, DeathLink, Range, Toggle, PerGameCommonOptions
 
+
 class DoorCost(Range):
     """Amount of Trinkets required to enter Areas. Set to 0 to disable artificial locks."""
     display_name = "Door Cost"
     range_start = 0
-    range_end = 3
+    range_end = 5
     default = 3
+
 
 class AreaCostRandomizer(Toggle):
     """Randomize which Area requires which set of DoorCost Trinkets"""
     display_name = "Area Cost Randomizer"
+
 
 class DeathLinkAmnesty(Range):
     """Amount of Deaths to take before sending a DeathLink signal, for balancing difficulty"""
@@ -20,13 +23,16 @@ class DeathLinkAmnesty(Range):
     range_end = 30
     default = 15
 
+
 class AreaRandomizer(Toggle):
     """Randomize Entrances to Areas"""
     display_name = "Area Randomizer"
 
+
 class MusicRandomizer(Toggle):
     """Randomize Music"""
     display_name = "Music Randomizer"
+
 
 @dataclass
 class V6Options(PerGameCommonOptions):
