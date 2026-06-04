dd2c9f08966b036547107049f492f43c968a5d61
desperandos101 <35909577+desperandos101@users.noreply.github.com>
2026-06-03 21:26:42 -0400

    0.6.80

--- worlds/terraria_desp_beta/__init__.py
+++ worlds/terraria_desp_beta/__init__.py
@@ -69,7 +69,7 @@ class TerrariaWorld(World):
     goal_items: Set[str]
     goal_locations: Set[str]
 
-    required_client_version = (0, 6, 70)
+    required_client_version = (0, 6, 80)
 
     def generate_early(self) -> None:
         goal, goal_locations = goals[self.options.goal.value]
